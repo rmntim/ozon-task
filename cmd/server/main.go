befb48adb2184a84b6558955a8decf25225ab495
Roman Timoshkin <rmntim@yandex.ru>
2024-05-23 09:11:44 +0300

    misc: setup logger

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/rmntim/ozon-task/intenal/config"
 )
@@ -14,6 +15,22 @@ const (
 func main() {
 	cfg, dbCfg := config.MustLoad()
 
-	fmt.Println(cfg)
-	fmt.Println(dbCfg)
+	log := setupLogger(cfg.Env)
+	log.Info("starting server", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+
+	_ = dbCfg
+}
+
+func setupLogger(env string) *slog.Logger {
+	var logger *slog.Logger
+
+	switch env {
+	case envLocal:
+		logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return logger
 }
