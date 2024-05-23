6dfc0b075c9e6fd19e4149236add2b9f7f466321
Roman Timoshkin <rmntim@yandex.ru>
2024-05-23 15:33:41 +0300

    fix: improve schema

--- migrations/postgres/000001_init_schema.up.sql
+++ migrations/postgres/000001_init_schema.up.sql
@@ -20,15 +20,9 @@ CREATE TABLE IF NOT EXISTS comments
 (
     id                SERIAL PRIMARY KEY,
     author_id         INT         NOT NULL REFERENCES users,
-    created_at        TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    likes             INT         NOT NULL DEFAULT 0,
+    created_at        TIMESTAMPTZ NOT NULL                      DEFAULT NOW(),
+    likes             INT         NOT NULL                      DEFAULT 0,
     content           TEXT        NOT NULL,
-    post_id           INT                  DEFAULT NULL,
-    parent_comment_id INT                  DEFAULT NULL,
-    FOREIGN KEY (post_id) REFERENCES posts ON DELETE CASCADE,
-    FOREIGN KEY (parent_comment_id) REFERENCES comments ON DELETE CASCADE,
-    CHECK (
-        (post_id IS NOT NULL AND parent_comment_id IS NULL) OR
-        (post_id IS NULL AND parent_comment_id IS NOT NULL)
-        )
+    post_id           INT         NOT NULL REFERENCES posts ON DELETE CASCADE,
+    parent_comment_id INT REFERENCES comments ON DELETE CASCADE DEFAULT NULL
 );
