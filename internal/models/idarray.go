f7b7bb1066e902863a3cb01ec323a5de326df801
Roman Timoshkin <rmntim@yandex.ru>
2024-05-24 17:54:52 +0300

    fix: actually parse pg array (not with json)

--- internal/models/idarray.go
+++ internal/models/idarray.go
@@ -2,7 +2,6 @@ package models
 
 import (
 	"database/sql/driver"
-	"encoding/json"
 	"errors"
 	"strconv"
 	"strings"
@@ -33,13 +32,13 @@ func (r *IDArray) Scan(src interface{}) (err error) {
 			*r = []uint{}
 			return
 		}
-		err = json.Unmarshal([]byte(src.(string)), &replies)
+		replies, err = parsePgArray(src.(string))
 	case []byte:
 		if src.([]byte)[1] == 'N' {
 			*r = []uint{}
 			return
 		}
-		err = json.Unmarshal(src.([]uint8), &replies)
+		replies, err = parsePgArray(string(src.([]byte)))
 	default:
 		err = errors.New("unknown type")
 	}
@@ -48,4 +47,17 @@ func (r *IDArray) Scan(src interface{}) (err error) {
 	}
 	*r = replies
 	return nil
+}
+
+func parsePgArray(array string) ([]uint, error) {
+	var replies []uint
+	nums := array[1 : len(array)-1]
+	for _, num := range strings.Split(nums, ",") {
+		id, err := strconv.Atoi(num)
+		if err != nil {
+			return nil, err
+		}
+		replies = append(replies, uint(id))
+	}
+	return replies, nil
 }
