a36e91d37d83455d62af696ba0c62127a4fb4c12
Roman Timoshkin <rmntim@yandex.ru>
2024-05-24 08:30:59 +0300

    feat: add query and mutation stubs

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,12 +1,23 @@
 package storage
 
 import (
+	"context"
 	"fmt"
+	"github.com/rmntim/ozon-task/graph/model"
 	"github.com/rmntim/ozon-task/internal/config"
 	"github.com/rmntim/ozon-task/internal/storage/postgres"
 )
 
 type Storage interface {
+	CreateUser(ctx context.Context, username string, email string, password string) (*model.User, error)
+	CreatePost(ctx context.Context, title string, content string, id uint) (*model.Post, error)
+	CreateComment(ctx context.Context, content string, id uint, id2 uint, id3 *uint) (*model.Comment, error)
+	GetUserById(ctx context.Context, id uint) (*model.User, error)
+	GetUsers(ctx context.Context) ([]*model.User, error)
+	GetPostById(ctx context.Context, id uint) (*model.Post, error)
+	GetPosts(ctx context.Context) ([]*model.Post, error)
+	GetCommentById(ctx context.Context, id uint) (*model.Comment, error)
+	GetComments(ctx context.Context) ([]*model.Comment, error)
 }
 
 // New creates new storage instance, depending on storage type.
